39d2d833d6f965e929479a138a8eb69a5c2658ad
ArsiRex <arsirex@yandex.ru>
2023-12-21 15:46:34 +0300

    Разбил страницу на компоненты, создал презентер для их отрисовки

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import TripInfoView from './view/trip-info-view.js';
+import FilterView from './view/filter-view.js';
+import BoardPresener from './presenter/board-presenter.js';
+import { render, RenderPosition } from './render.js';
+
+//const pageHeader = document.querySelector('.page-header');
+const tripEvents = document.querySelector('.trip-events');
+const tripMain = document.querySelector('.trip-main');
+const tripFilters = document.querySelector('.trip-controls__filters');
+const boardPresenter = new BoardPresener({container: tripEvents});
+
+render(new TripInfoView(), tripMain, RenderPosition.AFTERBEGIN);
+render(new FilterView(), tripFilters);
+boardPresenter.init();
