7c6861b54314e63ad9751eaf2464b39201db48c5
ArsiRex <arsirex@yandex.ru>
2024-01-23 17:45:42 +0300

    Добавит структуру данных фильтров

--- src/utils.js
+++ src/utils.js
@@ -8,5 +8,9 @@ function getRandomArrayElement(item) {
 const humanizePointsDate = (date) => dayjs(date).format(DATE_FORMAT);
 const humanizeShortDate = (date) => dayjs(date).format(SHORT_DATE_FORMAT).toUpperCase();
 const humanizeTime = (date) => dayjs(date).format(TIME_FORMAT);
-export { getRandomArrayElement, humanizePointsDate, humanizeShortDate, humanizeTime };
+const isFuture = (point) => dayjs().isBefore(point.startDate);
+const isPresent = (point) => dayjs().isAfter(point.startDate) && dayjs().isBefore(point.endDate);
+const isPast = (point) => dayjs().isAfter(point.endDate);
+
+export {isFuture, isPresent, isPast, getRandomArrayElement, humanizePointsDate, humanizeShortDate, humanizeTime };
 
