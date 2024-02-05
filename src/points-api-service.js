e6248e6e666ee170e908a485aa3fa340ac00e83d
ArsiRex <arsirex@yandex.ru>
2024-02-05 16:01:34 +0300

    Исправит points api service

--- src/points-api-service.js
+++ src/points-api-service.js
@@ -7,8 +7,15 @@ const Method = {
 
 export default class PointsApiService extends ApiService {
   get points() {
-    return this._load({url: 'points'})
-      .then(ApiService.parseResponse);
+    return this._load({url: 'points'}).then(ApiService.parseResponse);
+  }
+
+  get destinations() {
+    return this._load({url: 'destinations'}).then(ApiService.parseResponse);
+  }
+
+  get offers() {
+    return this._load({url: 'offers'}).then(ApiService.parseResponse);
   }
 
   async updatePoint(point) {
